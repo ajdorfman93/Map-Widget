fafb2995eace4d590623a98a6696ad3350af3369
ajdorfman93 <dorfmanart@gmail.com>
2024-12-09 09:33:14 -0500

    Initial commit

---
+++ page.js
@@ -0,0 +1,419 @@
+"use strict";
+
+/* global grist, window */
+
+let amap;
+let popups = {};
+let selectedTableId = null;
+let selectedRowId = null;
+let selectedRecords = null;
+let mode = 'multi';
+let mapSource = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}';
+let mapCopyright = 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012';
+
+// Required, Label value
+const Name = "Name";
+// Required
+const Longitude = "Longitude";
+// Required
+const Latitude = "Latitude";
+// Optional - switch column to trigger geocoding
+const Geocode = 'Geocode';
+// Optional - but required for geocoding. Field with address to find (might be formula)
+const Address = 'Address';
+// Optional - but useful for geocoding. Blank field which map uses
+// to store last geocoded Address.
+const GeocodedAddress = 'GeocodedAddress';
+
+let lastRecord;
+let lastRecords;
+
+//Color markers downloaded from leaflet repo, color-shifted to green
+//Used to show currently selected pin
+const selectedIcon =  new L.Icon({
+  iconUrl: 'marker-icon-green.png',
+  iconRetinaUrl: 'marker-icon-green-2x.png',
+  shadowUrl: 'marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+const defaultIcon =  new L.Icon.Default();
+
+
+// Creates clusterIcons that highlight if they contain selected row
+const selectedRowClusterIconFactory = function (selectedMarkerGetter) {
+  return function(cluster) {
+    var childCount = cluster.getChildCount();
+
+    let isSelected = false;
+    try {
+      const selectedMarker = selectedMarkerGetter();
+      isSelected = cluster.getAllChildMarkers().some((m) => m == selectedMarker);
+    } catch (e) {
+      console.error("WARNING: Error in clusterIconFactory in map widget");
+      console.error(e);
+    }
+
+    var c = ' marker-cluster-';
+    if (childCount < 10) {
+      c += 'small';
+    } else if (childCount < 100) {
+      c += 'medium';
+    } else {
+      c += 'large';
+    }
+
+    return new L.DivIcon({
+        html: '<div><span>'
+            + childCount
+            + ' <span aria-label="markers"></span>'
+            + '</span></div>',
+        className: 'marker-cluster' + c + (isSelected ? ' marker-cluster-selected' : ''),
+        iconSize: new L.Point(40, 40)
+    });
+  }
+};
+
+// Update the geocoder to handle Israeli addresses more accurately.
+// By specifying countrycodes='il', we focus searches on Israel.
+// Optionally, we can also set accept-language to 'he' for Hebrew results.
+let geocoder = L.Control.Geocoder && L.Control.Geocoder.nominatim({
+  geocodingQueryParams: {
+    countrycodes: 'il',        // Focus on Israel
+    'accept-language': 'he'    // Prefer Hebrew (optional)
+  }
+});
+
+if (URLSearchParams && location.search && geocoder) {
+  const c = new URLSearchParams(location.search).get('geocoder');
+  if (c && L.Control.Geocoder[c]) {
+    console.log('Using geocoder', c);
+    geocoder = L.Control.Geocoder[c]();
+  } else if (c) {
+    console.warn('Unsupported geocoder', c);
+  }
+  const m = new URLSearchParams(location.search).get('mode');
+  if (m) { mode = m; }
+}
+
+async function geocode(address) {
+  return new Promise((resolve, reject) => {
+    try {
+      geocoder.geocode(address, (v) => {
+        v = v[0];
+        if (v) { v = v.center; }
+        resolve(v);
+      });
+    } catch (e) {
+      console.log("Problem:", e);
+      reject(e);
+    }
+  });
+}
+
+async function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+// If widget has write access
+let writeAccess = true;
+// A ongoing scanning promise, to check if we are in progress.
+let scanning = null;
+
+async function scan(tableId, records, mappings) {
+  if (!writeAccess) { return; }
+  for (const record of records) {
+    if (!(Geocode in record)) { break; }
+    if (!record[Geocode]) { continue; }
+    const address = record.Address;
+    if (record[GeocodedAddress] && record[GeocodedAddress] !== record.Address) {
+      record[Longitude] = null;
+      record[Latitude] = null;
+    }
+    if (address && !record[Longitude]) {
+      const result = await geocode(address);
+      await grist.docApi.applyUserActions([ ['UpdateRecord', tableId, record.id, {
+        [mappings[Longitude]]: result ? result.lng : null,
+        [mappings[Latitude]]: result ? result.lat : null,
+        ...(GeocodedAddress in mappings) ? {[mappings[GeocodedAddress]]: address} : undefined
+      }] ]);
+      await delay(1000);
+    }
+  }
+}
+
+function scanOnNeed(mappings) {
+  if (!scanning && selectedTableId && selectedRecords) {
+    scanning = scan(selectedTableId, selectedRecords, mappings).then(() => scanning = null).catch(() => scanning = null);
+  }
+}
+
+function showProblem(txt) {
+  document.getElementById('map').innerHTML = '<div class="error">' + txt + '</div>';
+}
+
+function parseValue(v) {
+  if (typeof(v) === 'object' && v !== null && v.value && v.value.startsWith('V(')) {
+    const payload = JSON.parse(v.value.slice(2, v.value.length - 1));
+    return payload.remote || payload.local || payload.parent || payload;
+  }
+  return v;
+}
+
+function getInfo(rec) {
+  const result = {
+    id: rec.id,
+    name: parseValue(rec[Name]),
+    lng: parseValue(rec[Longitude]),
+    lat: parseValue(rec[Latitude])
+  };
+  return result;
+}
+
+// Function to clear last added markers.
+let clearMakers = () => {};
+
+let markers = [];
+
+function updateMap(data) {
+  data = data || selectedRecords;
+  selectedRecords = data;
+  if (!data || data.length === 0) {
+    showProblem("No data found yet");
+    return;
+  }
+  if (!(Longitude in data[0] && Latitude in data[0] && Name in data[0])) {
+    showProblem("Table does not yet have all expected columns: Name, Longitude, Latitude. You can map custom columns"+
+    " in the Creator Panel.");
+    return;
+  }
+
+  const tiles = L.tileLayer(mapSource, { attribution: mapCopyright });
+
+  const error = document.querySelector('.error');
+  if (error) { error.remove(); }
+  if (amap) {
+    try {
+      amap.off();
+      amap.remove();
+    } catch (e) {
+      // ignore
+      console.warn(e);
+    }
+  }
+  const map = L.map('map', {
+    layers: [tiles],
+    wheelPxPerZoomLevel: 90,
+  });
+
+  map.createPane('selectedMarker').style.zIndex = 620;
+  map.createPane('clusters').style.zIndex = 610;
+  map.createPane('otherMarkers').style.zIndex = 600;
+
+  const points = [];
+
+  popups = {};
+
+  markers = L.markerClusterGroup({
+    disableClusteringAtZoom: 18,
+    maxClusterRadius: 30,
+    showCoverageOnHover: true,
+    clusterPane: 'clusters',
+    iconCreateFunction: selectedRowClusterIconFactory(() => popups[selectedRowId]),
+  });
+
+  markers.on('click', (e) => {
+    const id = e.layer.options.id;
+    selectMaker(id);
+  });
+
+  for (const rec of data) {
+    const {id, name, lng, lat} = getInfo(rec);
+    if (String(lng) === '...') { continue; }
+    if (Math.abs(lat) < 0.01 && Math.abs(lng) < 0.01) {
+      continue;
+    }
+    const pt = new L.LatLng(lat, lng);
+    points.push(pt);
+
+    const marker = L.marker(pt, {
+      title: name,
+      id: id,
+      icon: (id == selectedRowId) ?  selectedIcon    :  defaultIcon,
+      pane: (id == selectedRowId) ? "selectedMarker" : "otherMarkers",
+    });
+
+    marker.bindPopup(name);
+    markers.addLayer(marker);
+
+    popups[id] = marker;
+  }
+  map.addLayer(markers);
+
+  clearMakers = () => map.removeLayer(markers);
+
+  try {
+    map.fitBounds(new L.LatLngBounds(points), {maxZoom: 15, padding: [0, 0]});
+  } catch (err) {
+    console.warn('cannot fit bounds');
+  }
+  function makeSureSelectedMarkerIsShown() {
+    const rowId = selectedRowId;
+
+    if (rowId && popups[rowId]) {
+      var marker = popups[rowId];
+      if (!marker._icon) { markers.zoomToShowLayer(marker); }
+      marker.openPopup();
+    }
+  }
+
+  amap = map;
+  makeSureSelectedMarkerIsShown();
+}
+
+function selectMaker(id) {
+   const previouslyClicked = popups[selectedRowId];
+   if (previouslyClicked) {
+     previouslyClicked.setIcon(defaultIcon);
+     previouslyClicked.pane = 'otherMarkers';
+   }
+   const marker = popups[id];
+   if (!marker) { return null; }
+
+   selectedRowId = id;
+
+   marker.setIcon(selectedIcon);
+   previouslyClicked.pane = 'selectedMarker';
+
+   markers.refreshClusters();
+
+   grist.setCursorPos?.({rowId: id}).catch(() => {});
+
+   return marker;
+}
+
+grist.on('message', (e) => {
+  if (e.tableId) { selectedTableId = e.tableId; }
+});
+
+function hasCol(col, anything) {
+  return anything && typeof anything === 'object' && col in anything;
+}
+
+function defaultMapping(record, mappings) {
+  if (!mappings) {
+    return {
+      [Longitude]: Longitude,
+      [Name]: Name,
+      [Latitude]: Latitude,
+      [Address]: hasCol(Address, record) ? Address : null,
+      [GeocodedAddress]: hasCol(GeocodedAddress, record) ? GeocodedAddress : null,
+      [Geocode]: hasCol(Geocode, record) ? Geocode : null,
+    };
+  }
+  return mappings;
+}
+
+function selectOnMap(rec) {
+  if (selectedRowId === rec.id) { return; }
+
+  selectedRowId = rec.id;
+  if (mode === 'single') {
+    updateMap([rec]);
+  } else {
+    updateMap();
+  }
+}
+
+grist.onRecord((record, mappings) => {
+  if (mode === 'single') {
+    lastRecord = grist.mapColumnNames(record) || record;
+    selectOnMap(lastRecord);
+    scanOnNeed(defaultMapping(record, mappings));
+  } else {
+    const marker = selectMaker(record.id);
+    if (!marker) { return; }
+    markers.zoomToShowLayer(marker);
+    marker.openPopup();
+  }
+});
+grist.onRecords((data, mappings) => {
+  lastRecords = grist.mapColumnNames(data) || data;
+  if (mode !== 'single') {
+    updateMap(lastRecords);
+    if (lastRecord) {
+      selectOnMap(lastRecord);
+    }
+    scanOnNeed(defaultMapping(data[0], mappings));
+  }
+});
+
+grist.onNewRecord(() => {
+  clearMakers();
+  clearMakers = () => {};
+})
+
+function updateMode() {
+  if (mode === 'single') {
+    selectedRowId = lastRecord.id;
+    updateMap([lastRecord]);
+  } else {
+    updateMap(lastRecords);
+  }
+}
+
+function onEditOptions() {
+  const popup = document.getElementById("settings");
+  popup.style.display = 'block';
+  const btnClose = document.getElementById("btnClose");
+  btnClose.onclick = () => popup.style.display = 'none';
+  const checkbox = document.getElementById('cbxMode');
+  checkbox.checked = mode === 'multi' ? true : false;
+  checkbox.onchange = async (e) => {
+    const newMode = e.target.checked ? 'multi' : 'single';
+    if (newMode != mode) {
+      mode = newMode;
+      await grist.setOption('mode', mode);
+      updateMode();
+    }
+  }
+  [ "mapSource", "mapCopyright" ].forEach((opt) => {
+    const ipt = document.getElementById(opt)
+    ipt.onchange = async (e) => {
+      await grist.setOption(opt, e.target.value);
+    }
+  })
+}
+
+const optional = true;
+grist.ready({
+  columns: [
+    "Name",
+    { name: "Longitude", type: 'Numeric'} ,
+    { name: "Latitude", type: 'Numeric'},
+    { name: "Geocode", type: 'Bool', title: 'Geocode', optional},
+    { name: "Address", type: 'Text', optional, optional},
+    { name: "GeocodedAddress", type: 'Text', title: 'Geocoded Address', optional},
+  ],
+  allowSelectBy: true,
+  onEditOptions
+});
+
+grist.onOptions((options, interaction) => {
+  writeAccess = interaction.accessLevel === 'full';
+  const newMode = options?.mode ?? mode;
+  mode = newMode;
+  if (newMode != mode && lastRecords) {
+    updateMode();
+  }
+  const newSource = options?.mapSource ?? mapSource;
+  mapSource = newSource;
+  document.getElementById("mapSource").value = mapSource;
+  const newCopyright = options?.mapCopyright ?? mapCopyright;
+  mapCopyright = newCopyright
+  document.getElementById("mapCopyright").value = mapCopyright;
+});
